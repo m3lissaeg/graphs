b7d93eebd9c259254a28dd3b38b4c114b263a279
M3lissaeg <melissa.escobar@utp.edu.co>
2022-11-15 21:41:33 -0500

    Agregar cambios. No funciona aún

--- graphComplete.py
+++ graphComplete.py
@@ -1,5 +1,6 @@
 import pygame, sys
 from pygame.locals import *
+
 pygame.init()
 
 WIDTH = 900
@@ -7,19 +8,34 @@ HEIGHT = 900
 
 pywindow = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('Draw Nodes for the graph')
-WHITE = (255, 255, 255)
-BLUE = (0, 0, 255)
-pywindow.fill(WHITE)
+font = pygame.font.Font('freesansbold.ttf', 32)
 img = pygame.image.load('graph.png')
+
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+BLUE = (0, 0, 255)
+DIAMETER = 30
+pywindow.fill(WHITE)
+
 listOfNodes = []
 listOfEdges = []
+rightClickStack = []
+currentName = [0]
 
 def nameGenerator():
-	return "A" # TODO. Improve this function 
+	# currentName +=1
+	return "A"
 
-def findNode(coordenates): #TODO this has to be improved
-	return "This is the node"
-
+def findNode(coordenates): 
+		radius = DIAMETER/2
+		for node in listOfNodes:
+			if ((coordenates[0] > node.coord[0] - radius) and
+					(coordenates[0] < node.coord[0] + radius) and
+					(coordenates[1] > node.coord[1] - radius) and
+					(coordenates[1] < node.coord[1] + radius) ):
+				return node
+			else:
+				return 'noNode'
 
 class Node(object):
 	def __init__(self, coord, name):
@@ -27,7 +43,12 @@ class Node(object):
 		self.name  = name
 
 	def draw(self):
-		pygame.draw.circle(pywindow, BLUE, (self.coord), 30, 1) #Draws a circle at the mouse position!
+		pygame.draw.circle(pywindow, BLUE, (self.coord), DIAMETER, 1) #Draws a circle at the mouse position!
+		textName = font.render(self.name, True, BLACK)
+		textNameRect = textName.get_rect()
+		textNameRect.center = self.coord
+		pywindow.blit(textName, textNameRect)
+
 
 class Edge(object):
 	def __init__(self, start, end):
@@ -35,9 +56,7 @@ class Edge(object):
 		self.end = end
 
 	def draw(self):
-		pygame.draw.line(pywindow, WHITE, (self.start), (self.end))
-
-
+		pygame.draw.line(pywindow, BLACK, (self.start.coord), (self.end.coord))
 
 
 if __name__ == '__main__':
@@ -54,14 +73,24 @@ if __name__ == '__main__':
 
 								if event.button == 1: # left click - draw node 
 									node = Node(position, nameGenerator())
+									print(node.name)
+									print(node.coord)
 									node.draw()
 									#Add the new node to the list of nodes
-									listOfNodes.append(node.name)
+									listOfNodes.append(node)
 								
 								if event.button == 3: # right click - draw edge 
-									nodeStart = findNode(position)
-									nodeEnd = findNode(position)
-									edge = Edge(nodeStart, nodeEnd)
+									# print(len(rightClickStack))
+									if len(rightClickStack) == 1:
+										print(rightClickStack[0])
+										nodeStart = findNode(position)
+										nodeEnd = findNode(rightClickStack.pop(0))
+										edge = Edge(nodeStart, nodeEnd)
+										print(nodeStart.coord)
+										# listOfEdges.append(edge)
+										# edge.draw()
+									else:
+										rightClickStack.append(position)
 
 
 					if event.type == QUIT:
